aea164e42304069bd54793fed06b89c843a62ce7
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-09-21 03:36:19 -0300

    trying to add I18n gem to the app so user can translate the page (en or pt-BR)... still in progress

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module RenatoJardimWebpage
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = %i[pt-BR en]
+    config.i18n.default_locale = :en
   end
 end
